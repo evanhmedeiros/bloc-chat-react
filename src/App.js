f25d9a71b042330e8fb0e82f3d1e622517615618
Evan Medeiros <kumbionics@gmail.com>
2018-08-09 21:57:04 -0400

    Checkpoint 2 list: completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+import React, { Component } from 'react';
+import RoomList from './components/RoomList';
+import './App.css';
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQdTiUsOnrgMxm6RGVGoVxkEUeQTkXc20",
+    authDomain: "bloc-chat-react-57b2f.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-57b2f.firebaseio.com",
+    projectId: "bloc-chat-react-57b2f",
+    storageBucket: "bloc-chat-react-57b2f.appspot.com",
+    messagingSenderId: "1037545036712"
+  };
+  firebase.initializeApp(config);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <RoomList firebase={firebase}/>
+      </div>
+    );
+  }
+}
+
+export default App;
