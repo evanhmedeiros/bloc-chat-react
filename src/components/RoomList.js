886838a104f050b3230563482fafa5f6f60199fc
Evan Medeiros <kumbionics@gmail.com>
2018-08-12 21:57:01 -0400

    Checkpoint 3: Create chat room: Completed

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -11,6 +11,15 @@ class RoomList extends Component {
 
     }
 
+    handleSubmit(newRoomName) {//this will allow newRoom to get pushed to firebase...
+        if (!this.state.newRoomName) {return} 
+        this.roomsRef.push({ name: newRoomName });
+        this.setState({ newRoomName: '' })
+    }
+
+    handleChange(e){//create a new room name
+        this.setState({newRoomName: e.target.value })
+    }
 
     componentDidMount() {
         this.roomsRef.on('child_added', snapshot => {
@@ -21,34 +30,32 @@ class RoomList extends Component {
     }
 
     //This function will push newRoom to firebase
-    roomCreate() {
-        this.roomsRef.push({name: this.state.newRoomName})
-    }
-    // user will enter text into input box, the value is goes to state.newRoomName
-    handleChange = (e) => {
-        this.setState({newRoomName: e.target.value})
-    }
+    // roomCreate() {
+    //     this.roomsRef.push({name: this.state.newRoomName})
+    // }
+    
     //this will launch roomCreate function
-    onClickHandle(e) {
-        e.preventDefault();
-        this.roomCreate()
-    }
+    // onClickHandle(e) {
+    //     e.preventDefault();
+    //     this.roomCreate()
+    // }
     
     
 
     render() {
         return(
             <div>
-                {/* form to create new room */}
                 {this.state.rooms.map(( room, index) => 
                 <div key={index}>{room.name}</div>)}
 
                 <form onSubmit={ (e) => { e.preventDefault(); 
-                    this.handleSubmit(this.state.newRoomName) }}>
-                </form>
-                {/* input to create new room, this will require event handlers... */}
-                <input type="text" value={this.state.newRoomName} onChange={ (e) => this.handleChange(e)}/>
+                this.handleSubmit(this.state.newRoomName) }
+                }>
+                
+                <input type="text" value={this.state.newRoomName} 
+                onChange={ (e) => this.handleChange(e)}/>
                 <input type="submit"/>
+                </form>
             </div>
         );
     }
